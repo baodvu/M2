e7ff2379697b4733980d69d0ee7037c54cc2d346
Justin Buchanan <justbuchanan@gmail.com>
2014-09-04 01:34:42 -0400

    filled out AntLabMain.java with my name and gtid

--- src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -63,10 +63,8 @@ import edu.gatech.oad.antlab.pkg2.*;
 		  //and gburdell with your gt id
 		  Person2 p2 = new Person2("P2");
 		  toPrint += p2.toString("gburdell2");
-		  //Person3 replace P3 with your name
-		  //and gburdell3 with your gt id
-		  Person3 p3 = new Person3("P3");
-		  toPrint += p3.toString("gburdell3");
+		  Person3 p3 = new Person3("Justin Buchanan");
+		  toPrint += p3.toString("jbuchanan30");
           //Person4 replace P4 with your name
           //and gburdell4 with your gt id
                   Person4 p4 = new Person4("Griffith");
